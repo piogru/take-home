351052b078c28b92cb279e4d055f0e145fab9c91
Piotr G <81086063+piogru@users.noreply.github.com>
2024-11-27 22:20:11 +0100

    docs: add choice/testing description in EntryPoint

--- src/components/Entrypoint.tsx
+++ src/components/Entrypoint.tsx
@@ -7,6 +7,25 @@ import { RefreshButton } from "./Refresh";
 import { ToggleButton } from "./Buttons";
 import { CardList } from "./CardList";
 
+/*
+  1. React-query is an async state manager, therefore I have decided to only save changes made by the user
+  in the Zustand store.
+  2. I have decided to disable refetchOnFocus due to randomized and shuffled data being returned from `useGetListData`.
+  3. I am assuming that "refresh" refers to a refetch since it is supposed to be implemented with react-query.
+  4. I am assuming that a card with 'isVisible: false' should not be shown in the deleted column
+
+  Testing:
+    I would primarily focus on integration tests of <EntryPoint /> component, in order to replicate regular
+    component as closely as possible and ensure that features provided by Zustand and React-query work correctly.
+    Mocks without randomized data might be necessary for proper testing.
+    In the case of <EntryPoint /> tests would include:
+      - Checking the number of elements rendered within the list and list length in the header,
+      - Clicking 'Refresh' and verifying if state is persisted properly,
+      - Checking if number of deleted cards is correct before revealing and if reveal causes proper number
+      of cards to be rendered,
+      - Checking if delete causes the correct card to be moved from one list to another,
+*/
+
 export const Entrypoint = () => {
   const [visibleCards, setVisibleCards] = useState<ListItem[]>([]);
   const [deletedCards, setDeletedCards] = useState<DeletedListItem[]>([]);
