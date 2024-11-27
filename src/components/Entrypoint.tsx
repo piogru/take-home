f351edf93e5286a9682cde26b91118d16daf8e19
Piotr G <81086063+piogru@users.noreply.github.com>
2024-11-27 20:21:44 +0100

    feat: implement reveal toggle for deleted cards

--- src/components/Entrypoint.tsx
+++ src/components/Entrypoint.tsx
@@ -6,11 +6,13 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { Card } from "./Card";
 import { Spinner } from "./Spinner";
 import { RefreshButton } from "./Refresh";
+import { ToggleButton } from "./Buttons";
 
 export const Entrypoint = () => {
   const [parent] = useAutoAnimate();
   const [visibleCards, setVisibleCards] = useState<ListItem[]>([]);
   const [deletedCards, setDeletedCards] = useState<DeletedListItem[]>([]);
+  const [revealDeletedCards, setRevealDeletedCards] = useState(false);
   const listQuery = useGetListData();
   const { deletedCardIdSet } = useStore(
     useShallow((state) => ({
@@ -50,13 +52,17 @@ export const Entrypoint = () => {
     setDeletedCards([]);
   }, [listQuery.data, listQuery.isLoading, deletedCardIdSet]);
 
+  const onRevealToggle = () => {
+    setRevealDeletedCards((prevState) => !prevState);
+  };
+
   if (listQuery.isLoading) {
     return <Spinner />;
   }
 
   return (
     <div className="flex gap-x-16">
-      <div className="w-72 max-w-xl bg-blue-500/50">
+      <div className="w-72 max-w-xl">
         <div className="flex items-center justify-between">
           <h1 className="mb-1 font-medium text-lg">
             My Awesome List ({visibleCards.length})
@@ -74,23 +80,22 @@ export const Entrypoint = () => {
           ))}
         </div>
       </div>
-      <div className="w-72 max-w-xl bg-slate-400">
+      <div className="w-72 max-w-xl">
         <div className="flex items-center justify-between">
           <h1 className="mb-1 font-medium text-lg">
             Deleted Cards ({deletedCards.length})
           </h1>
-          <button
-            disabled
-            className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1"
-          >
-            Reveal
-          </button>
+          <ToggleButton onClick={onRevealToggle}>
+            {revealDeletedCards ? "Hide" : "Reveal"}
+          </ToggleButton>
         </div>
-        <div className="flex flex-col gap-y-3">
-          {deletedCards.map((card) => (
-            <Card key={card.id} id={card.id} title={card.title} />
-          ))}
-        </div>
+        {revealDeletedCards ? (
+          <div className="flex flex-col gap-y-3">
+            {deletedCards.map((card) => (
+              <Card key={card.id} id={card.id} title={card.title} />
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   );
